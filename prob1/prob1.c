87130b27fd00f67d99dbad9a6578acb493be43f6
Charles A Garvin <cagarvin@google.com>
2013-08-13 13:48:00 -0700

    More option handling and brute_force method

--- prob1/prob1.c
+++ prob1/prob1.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
 #define _GNU_SOURCE
 #include <unistd.h>
 #include <getopt.h>
 
 #define DEFAULT_MAX   1000
+#define RANGE_MAX     100000
 
 typedef int (*multsum_fn)(int);
 
@@ -23,14 +26,39 @@ static void print_usage(const char *program)
     "    <method> is the computation method you wish to use,",
     "    brute_force, sieve or generate.  The default is brute_force.",
     "",
-    "    max is the maximum number (i.e. sum of all multiples below <max>.",
-    "    The default is 1000",
+    "    max is an integer representing the maximum number (i.e. sum of all",
+    "    multiples below <max>).  The value must be between 1 and 100000.  The",
+    "    default is 1000",
     NULL
   };
 
   printf("%s", program);
   for (i = 0; usage[i]; i++) {
     printf("%s\n", usage[i]);
+  }
+}
+
+static int brute_force(int max)
+{
+  int i;
+  int sum = 0;
+
+  for (i = 1; i < max; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      sum += i;
+    }
+  }
+
+  return sum;
+}
+
+static multsum_fn method_to_func(const char *name)
+{
+  if (!strcmp(name, "brute_force")) {
+    return brute_force;
+  }
+  else {
+    return NULL;
   }
 }
 
@@ -45,17 +73,32 @@ static int parse_opts(int argc, char **argv, struct my_opts *opts)
     {0,        0,                 0, 0}
   };
 
-  opts->func = NULL;
+  long val;
+  char *endp;
+
+  opts->func = brute_force;
   opts->max = DEFAULT_MAX;
 
   while ((opt = getopt_long(argc, argv, "", long_opts, &opt_ind)) >= 0) {
     switch (opt) {
       case 1:
-        printf("method = %s\n", optarg);
+        opts->func = method_to_func(optarg);
+        if (opts->func == NULL) {
+          printf("Invalid method name\n");
+          rv = -1;
+        }
         break;
 
       case 2:
-        printf("max = %s\n", optarg);
+        errno = 0;
+        val = strtol(optarg, &endp, 10);
+        if (errno || *endp || val < 1 || val > RANGE_MAX) {
+          printf("Invalid maximum number\n");
+          rv = -1;
+        }
+        else {
+          opts->max = val;
+        }
         break;
 
       default:
@@ -69,8 +112,7 @@ static int parse_opts(int argc, char **argv, struct my_opts *opts)
 
 int main(int argc, char **argv)
 {
-  int i;
-  int sum = 0;
+  int sum;
   struct my_opts opts;
 
   if (parse_opts(argc, argv, &opts) < 0) {
@@ -78,11 +120,7 @@ int main(int argc, char **argv)
     return EXIT_FAILURE;
   }
 
-  for (i = 1; i < opts.max; i++) {
-    if (i % 3 == 0 || i % 5 == 0) {
-      sum += i;
-    }
-  }
+  sum = opts.func(opts.max);
 
   printf("sum = %d\n", sum);
 
