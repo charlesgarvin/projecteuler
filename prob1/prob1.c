b8289cdb586a7e0d4787c2d2f52b3a86cb61d612
Charles A Garvin <cagarvin@google.com>
2013-08-13 13:12:35 -0700

    Add usage message and framework for command line options

--- prob1/prob1.c
+++ prob1/prob1.c
@@ -1,20 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define MAX   1000
 
-int main(void)
+void print_usage(const char *program)
+{
+  int i;
+  const char *usage[] = {
+    " [--method=<method>] [--max=<max>]",
+    "",
+    "    <method> is the computation method you wish to use,",
+    "    brute_force, sieve or generate.  The default is brute_force.",
+    "",
+    "    max is the maximum number (i.e. sum of all multiples below <max>.",
+    "    The default is 1000",
+    NULL
+  };
+
+  printf("%s", program);
+  for (i = 0; usage[i]; i++) {
+    printf("%s\n", usage[i]);
+  }
+}
+
+int parse_opts(int argc, char **argv)
+{
+  return -1;
+}
+
+int main(int argc, char **argv)
 {
   int i;
   int sum = 0;
 
+  if (parse_opts(argc, argv) < 0) {
+    print_usage(argv[0]);
+    return EXIT_FAILURE;
+  }
+
   for (i = 1; i < MAX; i++) {
     if (i % 3 == 0 || i % 5 == 0) {
-      printf("%d\n", i);
       sum += i;
     }
   }
 
   printf("sum = %d\n", sum);
 
-  return 0;
+  return EXIT_SUCCESS;
 }
